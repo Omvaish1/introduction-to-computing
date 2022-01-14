c2e7ad13370804e548e7fa581b50222aefb631e8
Om vaish <96487902+Omvaish1@users.noreply.github.com>
2022-01-14 12:07:25 +0530

    Add files via upload

---
+++ output asgn 1.py
@@ -0,0 +1,54 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Program 1  
+
+enter 1st number 40
+enter 2nd number 50
+enter 3rd number 60
+Sum of given nos is  150
+Average is  50.0 
+
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Program 2 
+
+ All values are to be entered in dollars.
+enter your gross income 500000
+enter no of dependents 5
+ The tax is  95000.0 
+
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Program 3  
+
+Student = [sid, name, gender, course, cgpa]
+enter your SID 21105087
+enter your name om 
+enter your gender 'M', 'F', 'U' for male, female and unknown respectively M
+enter your course name ECE
+enter your cgpa 9.5
+Student info  [21105087, 'om ', ' M', 'ECE', 9.5] 
+
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Program 4  
+
+enter marks of student 1 5
+enter marks of student 2 8
+enter marks of student 3 4
+enter marks of student 4 2
+enter marks of student 5 9
+Marks of five students are  [5, 8, 4, 2, 9]
+marks in ascending order  [2, 4, 5, 8, 9]
+marks in descending order  [9, 8, 5, 4, 2] 
+
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Program 5  
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+The modified list is =  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list is =  ['Red', 'Green', 'White', 'Purple', 'Yellow']
