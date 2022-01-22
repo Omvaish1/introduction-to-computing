02f869ef42688d5318acb86f9c9462cf44d26456
Om vaish <96487902+Omvaish1@users.noreply.github.com>
2022-01-23 01:53:16 +0530

    Add files via upload

---
+++ assignment 2/output asgn 2.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 1 
+
+length of the string = 35
+reversed order:  egaugnal evitisnes esac a si nohtyP
+New string:  a case sensitive
+Replaced string:  Python is object oriented language
+Index of substring 'a' in the input string is 10
+After removing white spaces the string is: Pythonisacasesensitivelanguage
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 2 
+
+Hey, Om Here!
+My SID is 21105087
+I am from ECE department and my CGPA is 9.500000
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 3 
+
+a&b : 8
+a|b : 58
+a^b : 50
+Left shift a and b with 2 bits: 224 40
+Right shift a with 2 bits and b with 4 bits: 14 0
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 4 
+
+Enter 1st number :35
+Enter 2nd number :36
+Enter 3rd number :34
+Greatest number : 36
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 5 
+
+Enter a string:NAME OM VAISH
+Yes
+
+============== RESTART: D:/engineering notes/assignments/output.py =============
+Problem 6 
+
+Length of side A:10
+Length of side B:6
+Length of side C:5
+Yes
