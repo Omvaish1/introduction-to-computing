a53ccf6e20483cc2c04bbcf2aa06f4119fcd1bd0
Om vaish <96487902+Omvaish1@users.noreply.github.com>
2022-02-14 13:37:38 +0530

    Add files via upload

---
+++ assignment 3/output asgn 3.py
@@ -0,0 +1,92 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 1
+Enter a string- mississippi
+Total occurances
+{'m': 1, 'i': 4, 's': 4, 'p': 2}
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 2
+enter the Day- 31
+enter the Month- 9
+enter the Year- 2020
+Entered date is not valid
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 2
+enter the Day- 31
+enter the Month- 12
+enter the Year- 2020
+Next Date is:  1 1 2021
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 3
+Enter the list:[1,2,3,4,5,6]
+Output: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36)]
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 4
+Enter grade score:9
+Your grade is 'A' and Excellent performance.
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 6
+Enter student name:om
+Enter student SID:21105087
+Enter Y to continue or N to end:y
+Enter student name:soumil
+Enter student SID:21105005
+Enter Y to continue or N to end:y
+Enter student name:vanshul
+Enter student SID:21105079
+Enter Y to continue or N to end:y
+Enter student name:vedansh
+Enter student SID:21105078
+Enter Y to continue or N to end:n
+The Dictionary containing {'SID':'Name'} is shown below
+{21105087: 'om', 21105005: 'soumil', 21105079: 'vanshul', 21105078: 'vedansh'}
+The Dictionary after sorting according to name:
+{21105087: 'om', 21105005: 'soumil', 21105079: 'vanshul', 21105078: 'vedansh'}
+The Dictionary after sorting according to SID:
+{21105005: 'soumil', 21105078: 'vedansh', 21105079: 'vanshul', 21105087: 'om'}
+Enter SID to get name of student:21105087
+Name of student with SID  21105087  is om
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 7
+Total elements of Fibonacci sequence that you want- 7
+Fibonnaci sequence with  7  terms
+1
+1
+2
+3
+5
+8
+13
+Average of total  7  terms of sequence is 4.714285714285714
+
+================= RESTART: D:\engineering notes\python\test.py =================
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3  {10, 7}
